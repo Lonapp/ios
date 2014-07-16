82fbcb37715c0bfc04c7ba052c517750585cea88
Kyle Ladd <kladd@barracuda.com>
2014-07-15 21:50:46 -0400

    Initial commit: Swift!

---
+++ Lon/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Lon
+//
+//  Created by Kyle Ladd on 7/15/14.
+//  Copyright (c) 2014 Lon Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet var label: UILabel
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
